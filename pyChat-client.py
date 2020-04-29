9905b924854b89ca161c696a571c284c79ed4b25
uzarel <32590908+Uzarel@users.noreply.github.com>
2020-04-29 17:19:52 +0200

    Improved code syntax and performance
    Improved code syntax by adding main() functions, added a cleanup function for the server and an handler for code execution termination

--- pyChat-client.py
+++ pyChat-client.py
@@ -1,23 +1,25 @@
 import sys
-import time
 import socket
 import colorama
 import datetime
 import threading
 
 def currentTime():
+    # Retrieves local time formatted as HH:MM:SS
     now = datetime.datetime.now()
     formattedTime = now.strftime("%H:%M:%S")
     return formattedTime
 
 def deleteLastLine():
+    # Writes ANSI codes to perform cursor movement and current line clear
     cursorUp = "\x1b[1A"
     eraseLine = "\x1b[2K"
     sys.stdout.write(cursorUp)
     sys.stdout.write(eraseLine)
 
 def send(sock):
-    while True:
+    # Handles sending messages to the server
+    while threadFlag:
         try:
             message = input()
             deleteLastLine()
@@ -26,35 +28,51 @@ def send(sock):
             print("An error occured while trying to send a message!")
             break
 
-def receive(sock, buffer = 1024):
-    while True:
+def receive(sock):
+    # Handles receiving messages from the server
+    while threadFlag:
         try:
-            message = sock.recv(buffer).decode()
+            message = sock.recv(2048).decode()
             if message:
                 print("[{}] {}".format(currentTime(), message))
             else:
-                # When connection errors occur, messages are empty
+                # When the server closes the socket, messages received are empty
                 break
         except:
             print("An error occured while trying to reach the server!")
             break
 
+def main():
+    # main() will refer to threadFlag as to the global variable defined globally
+    global threadFlag
+    # Colorama handles the ANSI escape codes to work also on Windows
+    colorama.init()
+    # The host and port of the chat server
+    host = "localhost"
+    port = 25000
+    # Creates the socket for a TCP application
+    socketFamily = socket.AF_INET
+    socketType = socket.SOCK_STREAM
+    clientSocket = socket.socket(socketFamily, socketType)
+    # Connects to the server
+    clientSocket.connect((host, port))
+    # Creates two threads for sending and receiving messages from the server
+    sendingThread = threading.Thread(target=send, args=(clientSocket,))
+    receivingThread = threading.Thread(target=receive, args=(clientSocket,))
+    # Start those threads
+    receivingThread.start()
+    sendingThread.start()
+    # Checks if both threads are alive for handling their termination
+    while receivingThread.is_alive() and sendingThread.is_alive():
+        continue
+    threadFlag = False
+    # Finally closes the socket object connection
+    clientSocket.close()
+    print("\nYou can now close the application.")
 
-# Colorama handles the ANSI escape codes to work on every platform
-colorama.init()
+# Flag used for threads termination
+threadFlag = True
 
-host = "localhost"
-port = 25000
-bufferSize = 2048
-socketFamily = socket.AF_INET
-socketType = socket.SOCK_STREAM
-clientSocket = socket.socket(socketFamily, socketType)
-clientSocket.connect((host, port))
-sendingThread = threading.Thread(target=send, args=(clientSocket,))
-receivingThread = threading.Thread(target=receive, args=(clientSocket, bufferSize))
-receivingThread.start()
-sendingThread.start()
-while receivingThread.is_alive() and sendingThread.is_alive():
-    time.sleep(1)
-clientSocket.close()
-print("\nYou can now close the application.")
+if __name__ == "__main__":
+    main()
+    pass
--- pyChat-server.py
+++ pyChat-server.py
@@ -1,17 +1,18 @@
+import atexit
 import socket
-from threading import Thread
+import threading
 
-def connectionThread():
+def connectionThread(sock):
     # Accepts a connection request and stores both a socket object and its IP address
     while True:
         try:
-            client, address = serverSocket.accept()
+            client, address = sock.accept()
         except:
             print("Something went wrong while accepting incoming connections!")
             break
         print("{} has connected.".format(address[0]))
         addresses[client] = address
-        Thread(target=clientThread, args=(client,)).start()
+        threading.Thread(target=clientThread, args=(client,)).start()
 
 def clientThread(client):
     # Handles the client
@@ -38,7 +39,7 @@ def clientThread(client):
     # Handles specific messages in a different way (user commands)
     while True:
         try:
-            message = client.recv(bufferSize).decode("utf8")
+            message = client.recv(2048).decode("utf8")
             if message == "/quit":
                 client.send("You left the chat!".encode("utf8"))
                 del addresses[client]
@@ -66,13 +67,13 @@ def clientThread(client):
 def getNickname(client):
     # Gets a nickname for a client (if it is not already taken)
     client.send("Welcome to pyChat! Please type your nickname:".encode("utf8"))
-    nickname = client.recv(bufferSize).decode("utf8")
+    nickname = client.recv(2048).decode("utf8")
     alreadyTaken = False
     if nickname in users.values():
         alreadyTaken = True
         while alreadyTaken:
             client.send("This nickname has already been taken. Please choose a different one:".encode("utf8"))
-            nickname = client.recv(bufferSize).decode("utf8")
+            nickname = client.recv(2048).decode("utf8")
             if nickname not in users.values():
                 alreadyTaken = False
     return nickname
@@ -92,42 +93,43 @@ def broadcast(message, sentBy = ""):
 def cleanup():
     # Closes all socket object connections
     if len(addresses) != 0:
-        for address in addresses:
-            address[0].close()
-            del addresses[address]
-            if address in users:
-                del users[address]
+        for sock in addresses.keys():
+            sock.close()
     print("Cleanup done.")
 
+def main():
+    # Register cleanup() as the function to be executed at termination
+    atexit.register(cleanup)
+    # The host and port for the chat service
+    host = ""
+    port = 25000
+    # Creates the socket for a TCP application
+    socketFamily = socket.AF_INET
+    socketType = socket.SOCK_STREAM
+    serverSocket = socket.socket(socketFamily, socketType)
+    # Binds the serverSocket at the specified port number
+    serverSocket.bind((host, port))
+    # Listens for up to 10 active connections
+    serverSocket.listen(10)
+    # Welcome message to the server owner
+    print("pyChat server is up and running!")
+    print("Listening for new connections on port {}.".format(port))
+
+    # Creates a thread for accepting incoming connections
+    connThread = threading.Thread(target=connectionThread, args=(serverSocket,))
+    connThread.start()
+    # Waits for it to end
+    connThread.join()
+    # Performs socket connections cleanup
+    cleanup()
+    # Closes the server socket object connection
+    serverSocket.close()
+    print("Server has shut down.")
 
 # Dictionaries of nicknames and addresses with socket object as key
 users = {}
 addresses = {}
 
-# Creates the socket for a TCP application
-socketFamily = socket.AF_INET
-socketType = socket.SOCK_STREAM
-serverSocket = socket.socket(socketFamily, socketType)
-# The host and port for the chat service
-host = ""
-port = 25000
-# The buffer size for the recv() method
-bufferSize = 2048
-# Binds the serverSocket at the specified port number
-serverSocket.bind((host, port))
-# Listens for up to 10 active connections
-serverSocket.listen(10)
-# Welcome message to the server owner
-print("pyChat server is up and running!")
-print("Listening for new connections on port {}.".format(port))
-
-# Creates a thread for accepting incoming connections
-connectionThread = Thread(target=connectionThread)
-connectionThread.start()
-# Waits for it to end and then closes the socket
-connectionThread.join()
-# Performs some connection and resources cleanup
-cleanup()
-# Closes the server socket object connection
-serverSocket.close()
-print("Server has shut down.")
+if __name__ == "__main__":
+    main()
+    pass
